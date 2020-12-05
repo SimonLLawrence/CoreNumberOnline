c3af789926f781fc4bd9d09125bcf1ca110b44d8
Simon Lawrence <sll@bjss.com>
2020-12-05 19:20:25 +0000

    test

--- CoreNumberAPI/CoreNumberAPI/Controllers/WeatherForecastController.cs
+++ CoreNumberAPI/CoreNumberAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace CoreNumberAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","leeds"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","leds"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
